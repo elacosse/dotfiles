6c8fbcfcedbcbe608214c3ec6fec466e6d86fa97
Eric Lacosse <eric.lacosse@mailbox.org>
2025-12-31 14:25:16 +0000

    remove pixi and poetry default

--- install.linux.sh
+++ install.linux.sh
@@ -20,37 +20,27 @@ chmod +x ./install_scripts/install_ohmyzsh.sh
 ./install_scripts/install_ohmyzsh.sh
 chmod -x ./install_scripts/install_ohmyzsh.sh
 
-# Step 7: Install poetry
-chmod +x ./install_scripts/install_poetry.sh
-./install_scripts/install_poetry.sh
-chmod -x ./install_scripts/install_poetry.sh
-
-# Step 8: Install pixi
-chmod +x ./install_scripts/install_pixi.sh
-./install_scripts/install_pixi.sh
-chmod -x ./install_scripts/install_pixi.sh
-
-# Step 9: Install uv
+# Step 7: Install uv
 chmod +x ./install_scripts/install_uv.sh
 ./install_scripts/install_uv.sh
 chmod -x ./install_scripts/install_uv.sh
 
-# Step 10: Now install zshrc from this repo
+# Step 8: Now install zshrc from this repo
 chmod +x ./install_scripts/install_dotfiles.sh
 ./install_scripts/install_dotfiles.sh
 chmod -x ./install_scripts/install_dotfiles.sh
 
-# Step 11: Install Gemini Code Assistant
+# Step 9: Install Gemini Code Assistant
 chmod +x ./install_scripts/install_code_assist.sh
 ./install_scripts/install_code_assist.sh
 chmod -x ./install_scripts/install_code_assist.sh
 
-# Step 12: Install cargo
+# Step 10: Install cargo
 chmod +x ./install_scripts/install_cargo.sh
 ./install_scripts/install_cargo.sh
 chmod -x ./install_scripts/install_cargo.sh
 
-# Step 13: Install zellij
+# Step 11: Install zellij
 chmod +x ./install_scripts/install_zellij.sh
 ./install_scripts/install_zellij.sh
 chmod -x ./install_scripts/install_zellij.sh
